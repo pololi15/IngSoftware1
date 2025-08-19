fdac03539f67804104ca451bfe5533815d5a1fe5
Ostin <ostinrivas15@gmail.com>
2025-08-18 23:55:29 -0400

    Agrege la funcionalidad de multiplicar apenas

--- src/presenter.js
+++ src/presenter.js
@@ -1,9 +1,16 @@
 import sumar from "./sumador";
+import multiplicar from "./multiplicador";
 
 const first = document.querySelector("#primer-numero");
 const second = document.querySelector("#segundo-numero");
 const form = document.querySelector("#sumar-form");
 const div = document.querySelector("#resultado-div");
+//
+const first2 = document.querySelector("#primer2-numero");
+const second2 = document.querySelector("#segundo2-numero"); 
+const form2 = document.querySelector("#multiplicar-form");
+const div2 = document.querySelector("#resultado2-div");
+
 
 form.addEventListener("submit", (event) => {
   event.preventDefault();
@@ -12,4 +19,13 @@ form.addEventListener("submit", (event) => {
   const secondNumber = Number.parseInt(second.value);
 
   div.innerHTML = "<p>" + sumar(firstNumber, secondNumber) + "</p>";
+});
+
+form2.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  const firstNumber = Number.parseInt(first2.value);
+  const secondNumber = Number.parseInt(second2.value);
+
+  div2.innerHTML = "<p>" + multiplicar(firstNumber, secondNumber) + "</p>";
 });
